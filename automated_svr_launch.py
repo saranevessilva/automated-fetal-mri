27fc2ae26d4c650885eac86104eed6695dd899c1
saransilva <sara.neves_silva@kcl.ac.uk>
2025-04-10 10:47:05 +0100

    trying multi-processing for svr

--- automated_svr_launch.py
+++ automated_svr_launch.py
@@ -78,6 +78,34 @@ def process_mrd_files(folder_path, in_group=None, out_folder=None):
             main(args)
 
 
+def run_svr(date_path: str):
+    command = f"""bash -c '
+    bash /home/auto-proc-svrtk/scripts/auto-brain-055t-reconstruction.sh \
+        /tmp/share/debug/{date_path}/nitfis \
+        /tmp/share/debug/{date_path}/{date_path}-result 1 4.5 1.0 1 &&
+    chmod 1777 -R /tmp/share/debug/{date_path}/{date_path}-result &&
+    /bin/MIRTK/build/lib/tools/pad-3d \
+        /tmp/share/debug/{date_path}/{date_path}-result/reo-SVR-output-brain.nii.gz \
+        /home/ref.nii.gz 160 1 &&
+    /bin/MIRTK/build/lib/tools/edit-image /home/ref.nii.gz /home/ref.nii.gz -dx 1 -dy 1 -dz 1 &&
+    /bin/MIRTK/build/lib/tools/transform-image \
+        /tmp/share/debug/{date_path}/{date_path}-result/reo-SVR-output-brain.nii.gz \
+        /tmp/share/debug/{date_path}/{date_path}-result/grid-reo-SVR-output-brain.nii.gz \
+        -target /home/ref.nii.gz -interp BSpline &&
+    /bin/MIRTK/build/lib/tools/nan \
+        /tmp/share/debug/{date_path}/{date_path}-result/grid-reo-SVR-output-brain.nii.gz 1000000 &&
+    /bin/MIRTK/build/lib/tools/convert-image \
+        /tmp/share/debug/{date_path}/{date_path}-result/grid-reo-SVR-output-brain.nii.gz \
+        /tmp/share/debug/{date_path}/{date_path}-result/grid-reo-SVR-output-brain.nii.gz -short &&
+    chmod 1777 /tmp/share/debug/{date_path}/{date_path}-result/grid-reo-SVR-output-brain.nii.gz &&
+    suffix=1;
+    while [ -d "/tmp/share/debug/{date_path}-$suffix" ]; do suffix=$((suffix+1)); done;
+    mv /tmp/share/debug/{date_path} /tmp/share/debug/{date_path}-$suffix;
+    mkdir /tmp/share/debug/{date_path};
+    chmod 1777 /tmp/share/debug/{date_path}"
+    """
+
+
 def process(connection, config, metadata):
     logging.info("Config: \n%s", config)
 
@@ -438,53 +466,29 @@ def process_image(images, connection, config, metadata):
     # os.environ['XAUTHORITY'] = '/home/sn21/.Xauthority'
     os.environ['XAUTHORITY'] = "/opt/code/automated-fetal-mri/.Xauthority"
 
-    # command = f'''docker run --rm --mount type=bind,source=/tmp/share/debug,target=/home/data \
-    # fetalsvrtk/svrtk:general_auto_amd sh -c 'bash /home/auto-proc-svrtk/scripts/auto-brain-055t-reconstruction.sh \
-    # /home/data/{date_path}/dicoms /home/data/{date_path}/{date_path}-result 1 4.5 1.0 1 ; \
-    # chmod 1777 -R /home/data/{date_path}/{date_path}-result ; \
-    # /bin/MIRTK/build/lib/tools/pad-3d /home/data/{date_path}/{date_path}-result/reo-SVR-output-brain.nii.gz /home/ref.nii.gz 160 1 ; \
+    # command = f"""bash /home/auto-proc-svrtk/scripts/auto-brain-055t-reconstruction.sh \
+    # /tmp/share/debug/{date_path}/nitfis /tmp/share/debug/{date_path}/{date_path}-result 1 4.5 1.0 1 ; \
+    # chmod 1777 -R /tmp/share/debug/{date_path}/{date_path}-result ; \
+    # /bin/MIRTK/build/lib/tools/pad-3d /tmp/share/debug/{date_path}/{date_path}-result/reo-SVR-output-brain.nii.gz /home/ref.nii.gz 160 1 ; \
     # /bin/MIRTK/build/lib/tools/edit-image /home/ref.nii.gz /home/ref.nii.gz -dx 1 -dy 1 -dz 1 ; \
-    # /bin/MIRTK/build/lib/tools/transform-image /home/data/{date_path}/{date_path}-result/reo-SVR-output-brain.nii.gz \
-    # /home/data/{date_path}/{date_path}-result/grid-reo-SVR-output-brain.nii.gz -target /home/ref.nii.gz -interp BSpline ; \
-    # /bin/MIRTK/build/lib/tools/nan /home/data/{date_path}/{date_path}-result/grid-reo-SVR-output-brain.nii.gz 1000000 ; \
-    # /bin/MIRTK/build/lib/tools/convert-image /home/data/{date_path}/{date_path}-result/grid-reo-SVR-output-brain.nii.gz \
-    # /home/data/{date_path}/{date_path}-result/grid-reo-SVR-output-brain.nii.gz -short ; \
-    # chmod 1777 /home/data/{date_path}/{date_path}-result/grid-reo-SVR-output-brain.nii.gz ; \
-    # bash /home/auto-proc-svrtk/scripts/auto-body-055t-reconstruction.sh /home/data/{date_path}/dicoms \
-    # /home/data/{date_path}/{date_path}-result 1 4.5 1.0 1 ; \
-    # chmod 1777 -R /home/data/{date_path}/{date_path}-result ; \
-    # /bin/MIRTK/build/lib/tools/pad-3d /home/data/{date_path}/{date_path}-result/reo-DSVR-output-body.nii.gz /home/ref.nii.gz 256 1 ; \
-    # /bin/MIRTK/build/lib/tools/edit-image /home/ref.nii.gz /home/ref.nii.gz -dx 1 -dy 1 -dz 1 ; \
-    # /bin/MIRTK/build/lib/tools/transform-image /home/data/{date_path}/{date_path}-result/reo-DSVR-output-body.nii.gz \
-    # /home/data/{date_path}/{date_path}-result/grid-reo-DSVR-output-body.nii.gz -target /home/ref.nii.gz -interp BSpline ; \
-    # /bin/MIRTK/build/lib/tools/nan /home/data/{date_path}/{date_path}-result/grid-reo-DSVR-output-body.nii.gz 1000000 ; \
-    # /bin/MIRTK/build/lib/tools/convert-image /home/data/{date_path}/{date_path}-result/grid-reo-DSVR-output-body.nii.gz \
-    # /home/data/{date_path}/{date_path}-result/grid-reo-DSVR-output-body.nii.gz -short ; \
-    # chmod 1777 /home/data/{date_path}/{date_path}-result/grid-reo-DSVR-output-body.nii.gz ; \
+    # /bin/MIRTK/build/lib/tools/transform-image /tmp/share/debug/{date_path}/{date_path}-result/reo-SVR-output-brain.nii.gz \
+    # /tmp/share/debug/{date_path}/{date_path}-result/grid-reo-SVR-output-brain.nii.gz -target /home/ref.nii.gz -interp BSpline ; \
+    # /bin/MIRTK/build/lib/tools/nan /tmp/share/debug/{date_path}/{date_path}-result/grid-reo-SVR-output-brain.nii.gz 1000000 ; \
+    # /bin/MIRTK/build/lib/tools/convert-image /tmp/share/debug/{date_path}/{date_path}-result/grid-reo-SVR-output-brain.nii.gz \
+    # /tmp/share/debug/{date_path}/{date_path}-result/grid-reo-SVR-output-brain.nii.gz -short ; \
+    # chmod 1777 /tmp/share/debug/{date_path}/{date_path}-result/grid-reo-SVR-output-brain.nii.gz ; \
     # suffix=1; \
-    # while [ -d "/home/data/{date_path}-$suffix" ]; do suffix=$((suffix+1)); done; \
-    # mv /home/data/{date_path} /home/data/{date_path}-$suffix; \
-    # mkdir /home/data/{date_path}; \
-    # chmod 1777 /home/data/{date_path}; ' '''
+    # while [ -d "/tmp/share/debug/{date_path}-$suffix" ]; do suffix=$((suffix+1)); done; \
+    # mv /tmp/share/debug/{date_path} /tmp/share/debug/{date_path}-$suffix; \
+    # mkdir /tmp/share/debug/{date_path}; \
+    # chmod 1777 /tmp/share/debug/{date_path}; """
+    #
+    # subprocess.run(command, shell=True, executable="/bin/bash", capture_output=True, text=True)
 
-    command = f"""bash /home/auto-proc-svrtk/scripts/auto-brain-055t-reconstruction.sh \
-    /tmp/share/debug/{date_path}/nitfis /tmp/share/debug/{date_path}/{date_path}-result 1 4.5 1.0 1 ; \
-    chmod 1777 -R /tmp/share/debug/{date_path}/{date_path}-result ; \
-    /bin/MIRTK/build/lib/tools/pad-3d /tmp/share/debug/{date_path}/{date_path}-result/reo-SVR-output-brain.nii.gz /home/ref.nii.gz 160 1 ; \
-    /bin/MIRTK/build/lib/tools/edit-image /home/ref.nii.gz /home/ref.nii.gz -dx 1 -dy 1 -dz 1 ; \
-    /bin/MIRTK/build/lib/tools/transform-image /tmp/share/debug/{date_path}/{date_path}-result/reo-SVR-output-brain.nii.gz \
-    /tmp/share/debug/{date_path}/{date_path}-result/grid-reo-SVR-output-brain.nii.gz -target /home/ref.nii.gz -interp BSpline ; \
-    /bin/MIRTK/build/lib/tools/nan /tmp/share/debug/{date_path}/{date_path}-result/grid-reo-SVR-output-brain.nii.gz 1000000 ; \
-    /bin/MIRTK/build/lib/tools/convert-image /tmp/share/debug/{date_path}/{date_path}-result/grid-reo-SVR-output-brain.nii.gz \
-    /tmp/share/debug/{date_path}/{date_path}-result/grid-reo-SVR-output-brain.nii.gz -short ; \
-    chmod 1777 /tmp/share/debug/{date_path}/{date_path}-result/grid-reo-SVR-output-brain.nii.gz ; \
-    suffix=1; \
-    while [ -d "/tmp/share/debug/{date_path}-$suffix" ]; do suffix=$((suffix+1)); done; \
-    mv /tmp/share/debug/{date_path} /tmp/share/debug/{date_path}-$suffix; \
-    mkdir /tmp/share/debug/{date_path}; \
-    chmod 1777 /tmp/share/debug/{date_path}; """
+    gpu_proc = Process(target=run_svr, args=(debugFolder,))
+    gpu_proc.start()
 
-    subprocess.run(command, shell=True, executable="/bin/bash", capture_output=True, text=True)
+    # run_volumetry_pipeline(debugFolder)  # Not in a separate process
 
     print()
     print("--------------------------------------------------------------")
