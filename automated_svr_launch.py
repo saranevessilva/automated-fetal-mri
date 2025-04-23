21ed5637883b11e55acdcb84bdd65ba0bd6b4281
Sara Neves Silva <Sara.Neves_Silva@kcl.ac.uk>
2025-04-23 14:39:11 +0100

    re-structuring the launch of the docker

--- automated_svr_launch.py
+++ automated_svr_launch.py
@@ -459,34 +459,18 @@ def process_image(images, connection, config, metadata):
                 os.remove(file_path)
             except Exception as e:
                 print(f"Error deleting file {file_name}: {e}")
-
-    print("Launching docker now...")
-
-    # Set the DISPLAY and XAUTHORITY environment variables
-    os.environ['DISPLAY'] = ':1'  # Replace with your X11 display, e.g., ':1.0'
-    # os.environ['XAUTHORITY'] = '/home/sn21/.Xauthority'
-    os.environ['XAUTHORITY'] = "/opt/code/automated-fetal-mri/.Xauthority"
-
-    # command = f"""bash /home/auto-proc-svrtk/scripts/auto-brain-055t-reconstruction.sh \ /tmp/share/debug/{
-    # date_path}/nitfis /tmp/share/debug/{date_path}/{date_path}-result 1 4.5 1.0 1 ; \ chmod 1777 -R
-    # /tmp/share/debug/{date_path}/{date_path}-result ; \ /bin/MIRTK/build/lib/tools/pad-3d /tmp/share/debug/{
-    # date_path}/{date_path}-result/reo-SVR-output-brain.nii.gz /home/ref.nii.gz 160 1 ; \
-    # /bin/MIRTK/build/lib/tools/edit-image /home/ref.nii.gz /home/ref.nii.gz -dx 1 -dy 1 -dz 1 ; \
-    # /bin/MIRTK/build/lib/tools/transform-image /tmp/share/debug/{date_path}/{
-    # date_path}-result/reo-SVR-output-brain.nii.gz \ /tmp/share/debug/{date_path}/{
-    # date_path}-result/grid-reo-SVR-output-brain.nii.gz -target /home/ref.nii.gz -interp BSpline ; \
-    # /bin/MIRTK/build/lib/tools/nan /tmp/share/debug/{date_path}/{date_path}-result/grid-reo-SVR-output-brain.nii.gz
-    # 1000000 ; \ /bin/MIRTK/build/lib/tools/convert-image /tmp/share/debug/{date_path}/{
-    # date_path}-result/grid-reo-SVR-output-brain.nii.gz \ /tmp/share/debug/{date_path}/{
-    # date_path}-result/grid-reo-SVR-output-brain.nii.gz -short ; \ chmod 1777 /tmp/share/debug/{date_path}/{
-    # date_path}-result/grid-reo-SVR-output-brain.nii.gz ; \ suffix=1; \ while [ -d "/tmp/share/debug/{
-    # date_path}-$suffix" ]; do suffix=$((suffix+1)); done; \ mv /tmp/share/debug/{date_path} /tmp/share/debug/{
-    # date_path}-$suffix; \ mkdir /tmp/share/debug/{date_path}; \ chmod 1777 /tmp/share/debug/{date_path}; """
     #
-    # subprocess.run(command, shell=True, executable="/bin/bash", capture_output=True, text=True)
+    # print("Launching docker now...")
+    #
+    # gpu_proc = Process(target=run_svr, args=(debugFolder,))
+    # gpu_proc.start()
 
-    gpu_proc = Process(target=run_svr, args=(debugFolder,))
-    gpu_proc.start()
+    if __name__ == "__main__":
+        print("Launching docker now...")
+        gpu_proc = Process(target=run_svr, args=(debugFolder,))
+        gpu_proc.start()
+        gpu_proc.join()  # Optional but useful for debugging
+        print("SVR process finished")
 
     # run_volumetry_pipeline(debugFolder)  # Not in a separate process
 
